cd89cfec2d8057e77632f95821f561b208d1f9d2
Jaydeep Sambangi <jcsambangi@gmail.com>
2018-11-13 13:38:56 -0500

    completed server handler functions

--- server.py
+++ server.py
@@ -18,6 +18,7 @@ def new_patient():
     new_patient_data = request.get_json()
     from outsource import add_new_patient
     patientRecord = add_new_patient(new_patient_data, patientRecord)
+    return jsonify(patientRecord)
 
 
 @app.route("/api/heart_rate/<patient_id>", methods=["GET", "POST"])
@@ -28,6 +29,15 @@ def heart_rate(patient_id):
     :param patient_id: patient id as string
     :param heart_rate: only if POSTing - heart rate as integer
     """
+    """EMAIL"""
+    if request.method == 'POST':
+        new_HR = request.get_json()
+        from outsource import add_heart_rate
+        patientRecord = add_heart_rate(new_HR)
+        return jsonify(patientRecord)
+    else:
+        from outsource import get_heart_rates
+        return jsonify(get_heart_rates(patient_id))
 
 
 @app.route("/api/status/<patient_id>", methods=["GET"])
@@ -36,6 +46,8 @@ def status(patient_id):
 
     :param patient_id: patient id as string
     """
+    from outsource import get_status
+    return jsonify(get_status(patient_id))
 
 
 @app.route("/api/heart_rate/average/<patient_id>", methods=["GET"])
@@ -44,6 +56,8 @@ def average(patient_id):
 
     :param patient_id: patient id as string
     """
+    from outsource import get_average
+    return jsonify(get_average(patient_id))
 
 
 @app.route("/api/heart_rate/interval_average", methods=["POST"])
@@ -53,6 +67,9 @@ def interval_average():
     :param patient_id: patient id as string
     :param heart_rate_average_since: timestamp as string
     """
+    request_interval_average = request.get_json()
+    from outsource import get_interval_average
+    return jsonify(get_interval_average(request_interval_average))
 
 
 if __name__ == "__main__":
